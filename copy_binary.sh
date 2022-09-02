9dcfa383540719c89e7409c6d4f9a747a7f72d48
Zhiyuan <zydong829@sjtu.edu.cn>
2022-09-02 12:10:27 +0000

    refine

--- copy_binary.sh
+++ copy_binary.sh
@@ -1,1 +1,7 @@
 scp -r bin/ 172.31.25.193:~/calvin/
+scp -r bin/ 172.31.18.225:~/calvin/
+scp -r bin/ 172.31.24.38:~/calvin/
+scp -r bin/ 172.31.30.244:~/calvin/
+scp -r bin/ 172.31.23.221:~/calvin/
+scp -r bin/ 172.31.29.219:~/calvin/
+scp -r bin/ 172.31.20.95:~/calvin/
